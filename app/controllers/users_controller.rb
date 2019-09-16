5630a185e3ee7caf7cdbda0d4f6c85ca8f36eb33
Bach <hoangbachilt@gmail.com>
2019-09-16 18:25:29 +0700

    feat(signup): Sign up, Sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -8,17 +8,43 @@ class UsersController < ApplicationController
     @user= User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
     @user = User.find(params[:id])
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Successfully create"
     else
-      render 'edit'
+      flash[:danger] = "Error"
+      render "new"
     end
+  end
+
+  def update
+    @user.update_attribute(user_params)
+    if @user.update
+      flash[:success] = "Update profile successfully"
+    else
+      render "eidt"
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 end
